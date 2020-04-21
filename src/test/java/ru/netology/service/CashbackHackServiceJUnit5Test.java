61882a64061e83eb78f5693a2d6716d22871175c
Lina <linokarlino@mail.ru>
2020-04-21 18:34:38 +0300

    Added JUnit5 tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceJUnit5Test.java
@@ -0,0 +1,28 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceJUnit5Test {
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void testRemain() {
+        int amount = 1400;
+        int actual = service.remain(amount);
+        int expected = 600;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainZero() {
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+}
